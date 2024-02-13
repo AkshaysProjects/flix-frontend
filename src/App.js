3d6c4f772a97499a84b1d7a34b2675b426ec2a59
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2024-02-13 19:49:39 +0530

    Add dummy boiler places for home, login and register pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <>
+      <Routes>
+        {" "}
+        <Route path="/" element=<Home /> />
+        <Route path="/login" element=<Login /> />
+        <Route path="/register" element=<Register /> />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
